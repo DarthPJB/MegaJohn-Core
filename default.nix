a0ed7b65df9f752132a1165dfb0315da7289b2e5
john bargman <darthpjb@gmail.com>
2021-03-29 22:49:07 +0100

    updated gitignore and submodules

--- default.nix
+++ default.nix
@@ -14,6 +14,7 @@ emscriptenStdenv.mkDerivation
     repo = "MegaJohn-Core";
     rev = "fc382262b9503236ec54c21413581e4f5bfa25e8";
     sha256 = "0qkmcgf70yfbwsgr9xc92bdswkd9m0hfr3y4nyqp1mygag0n9w6s";
+    fetchSubmodules = true;
     #nix-prefetch-url --unpack https://github.com/DarthPJB/MegaJohn-Core/archive/fc382262b9503236ec54c21413581e4f5bfa25e8.tar.gz
   };
   buildInputs = [ pkgs.figlet ];
--- shell.nix
+++ shell.nix
@@ -5,10 +5,11 @@
 # Generate Shell
 pkgs.mkShell
 {
-  buildInputs = [ pkgs.emscriptenStdenv ];
+  buildInputs = [ pkgs.emscriptenStdenv pkgs.simplehttp2server];
 
   #Run build-task post generation (TODO: makefile)
   shellHook = ''
+    simplehttp2server -listen localhost:5000&
      ./task.sh;
   '';
 }
