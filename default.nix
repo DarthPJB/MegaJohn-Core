c6ffcc1b3661e790c89edd68556135d320822b39
John Bargman <darthpjb@gmail.com>
2021-08-17 13:54:14 +0100

    implemented a theoretical framework for direct compilation as a trial - not currently building

--- default.nix
+++ default.nix
@@ -20,20 +20,23 @@ stdenvNoCC.mkDerivation
 
   autoreconfPhase = " echo autoreconfPhase not used...";
   configurePhase =  " echo configure not used";
-  buildInputs = [ pkgs.figlet pkgs.emscripten];
+  buildInputs = [ pkgs.clang pkgs.wabt pkgs.binaryen pkgs.llvm pkgs.figlet pkgs.emscripten];
 
   buildPhase = ''
-  HOME=$TMPDIR
   mkdir $out
-  emcc -O3 src/main.cpp -o $out/main.wasm
+  cd $src/src
+  clang++ -emit-llvm --target=wasm32 -S main.cpp -o $TMPDIR/main.ll
+  llc -march=wasm32 -filetype=asm $TMPDIR/main.ll -o $TMPDIR/main.s
+  s2wasm $TMPDIR/main.s -o $TMPDIR/main.wat
+  wat2wasm $TMPDIR/main.wat -o $out/main.wasm
   '';
+# Alternately to emscripten - https://github.com/tpimh/wasm-toolchain/blob/master/c2wasm
+#  HOME=$TMPDIR
+#emcc -O3 src/main.cpp -o $out/main.wasm
 
-  # Alternately to emscripten - https://github.com/tpimh/wasm-toolchain/blob/master/c2wasm
-# using  wabt binaryen llvm
-  installPhase = " echo Looool";
+  installPhase = " echo nothing to install";
 
-  #Run build-task post generation (TODO: makefile)
   shellHook = ''
-
+      echo no shell hook
   '';
 }
