f7d9dd1e810358754c5a4af7f91c10c7f53f4a5c
John Bargman <darthpjb@gmail.com>
2021-08-17 11:29:54 +0100

    Created default.nix as per nix-build specification. Attempting to use emscripten is inefficient, cache generation is wasteful.

--- default.nix
+++ default.nix
@@ -1,28 +1,39 @@
-# with import <nixpkgs> {}; callPackage /path/to/package.nix {}
+with import <nixpkgs> {};
 
 # default nixpkgs
-{ emscriptenStdenv, fetchFromGitHub, figlet, pkgs  }:
+#{ emscriptenStdenv, fetchFromGitHub, figlet, pkgs  }:
 
 
 # Generate Shell
-emscriptenStdenv.mkDerivation
+stdenvNoCC.mkDerivation
 {
   name = "MegaJohn-Core";
-  builder = ./task.sh;
-  src = fetchFromGitHub {
-    owner = "DarthPJB";
-    repo = "MegaJohn-Core";
-    rev = "a0ed7b65df9f752132a1165dfb0315da7289b2e5";
-    sha256 = "1vrshq1z6i8s3zs41bs2v2pldfax26sv6fv5w4ci80wfqglcfmdz";
-    fetchSubmodules = true;
-    #nix-prefetch-url --unpack https://github.com/DarthPJB/MegaJohn-Core/archive/a0ed7b65df9f752132a1165dfb0315da7289b2e5.tar.gz
-  };
-  buildInputs = [ pkgs.figlet ];
+#  builder = ./task.sh;
+#  src = fetchFromGitHub {
+#    owner = "DarthPJB";
+#    repo = "MegaJohn-Core";
+#    rev = "a0ed7b65df9f752132a1165dfb0315da7289b2e5";
+#sha256 = "1vrshq1z6i8s3zs41bs2v2pldfax26sv6fv5w4ci80wfqglcfmdz";
+#    fetchSubmodules = true;
+#  };
+  src = ./.;
+
+  autoreconfPhase = " echo autoreconfPhase not used...";
+  configurePhase =  " echo configure not used";
+  buildInputs = [ pkgs.figlet pkgs.emscripten];
+
+  buildPhase = ''
+  HOME=$TMPDIR
+  mkdir $out
+  emcc -O3 src/main.cpp -o $out/main.wasm
+  '';
+
+  # Alternately to emscripten - https://github.com/tpimh/wasm-toolchain/blob/master/c2wasm
+# using  wabt binaryen llvm
+  installPhase = " echo Looool";
 
   #Run build-task post generation (TODO: makefile)
   shellHook = ''
-    export EMSCRIPTEN_ROOT_PATH='${pkgs.emscripten}/share/emscripten'
-    export EMSCRIPTEN='${pkgs.emscripten}/share/emscripten'
-    figlet "emscripten build enviroment ready:"
+
   '';
 }
--- task.sh
+++
@@ -1,4 +0,0 @@
-#empty - for now
-figlet Here we go!
-# Provide a sweet micro-server to provide the basic page.
-#simplehttp2server -listen localhost:5000
