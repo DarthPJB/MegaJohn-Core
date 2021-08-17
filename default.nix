fbd35d7caebe0bdcae57fa9a36e1a4adeeaac25b
John Bargman <darthpjb@gmail.com>
2021-08-17 14:06:44 +0100

    added more relevant comments as to this branch in default.nix

--- default.nix
+++ default.nix
@@ -22,6 +22,13 @@ stdenvNoCC.mkDerivation
   configurePhase =  " echo configure not used";
   buildInputs = [ pkgs.clang pkgs.wabt pkgs.binaryen pkgs.llvm pkgs.figlet pkgs.emscripten];
 
+# the intention here is to eliminate the use of emscriptenStdenv and instead
+# produce a very simple 'pure' C++ -> wasm pipeline.
+# Wasmer might be a better option, or using the nix-stdenvwasm paradime.
+# for the moment I am even trying to avoid cmake-emcmake entirely.
+# this may be a dead-branch, but fundementally I belive in K.I.S.S
+
+# Alternately to emscripten - Based on github.com/tpimh/wasm-toolchain/blob/master/c2wasm
   buildPhase = ''
   mkdir $out
   cd $src/src
@@ -30,13 +37,12 @@ stdenvNoCC.mkDerivation
   s2wasm $TMPDIR/main.s -o $TMPDIR/main.wat
   wat2wasm $TMPDIR/main.wat -o $out/main.wasm
   '';
-# Alternately to emscripten - https://github.com/tpimh/wasm-toolchain/blob/master/c2wasm
-#  HOME=$TMPDIR
-#emcc -O3 src/main.cpp -o $out/main.wasm
+
+
 
   installPhase = " echo nothing to install";
 
   shellHook = ''
-      echo no shell hook
+      figlet Hello Compiled!
   '';
 }
